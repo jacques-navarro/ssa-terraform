b29ba32e3bdc64539b85988f5efe02568c00d254
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-05-30 09:55:08 +0200

    Add template for 42. HOL Private Subnet with NAT Gateway
    This lab requires you to ssh into an EC2 instance that is in a public
    subnet and then ssh into a another instance that is in a private subnet.
    
    Once you are logged into the private instance, you must ping an outside
    server to prove the NAT gateway is able to direct traffic outside the
    VPC.

---
+++ 42-private-subnet-with-gateway/main.tf
@@ -0,0 +1,160 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "nat-gateway" {
+  cidr_block         = "10.0.0.0/16"
+  enable_dns_support = true
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_internet_gateway" "nat-gateway" {
+  vpc_id = aws_vpc.nat-gateway.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_subnet" "nat-gateway-public" {
+  vpc_id            = aws_vpc.nat-gateway.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+
+# private subnet, security group, ingress rule, nat-gateway, route table association, route table, instance
+
+resource "aws_subnet" "nat-gateway-private" {
+  vpc_id            = aws_vpc.nat-gateway.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_security_group" "nat-gateway" {
+  name   = "nat-gateway-ssh"
+  vpc_id = aws_vpc.nat-gateway.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh-rule" {
+  security_group_id = aws_security_group.nat-gateway.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "ssh-rule" {
+  security_group_id = aws_security_group.nat-gateway.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 22
+  ip_protocol = "tcp"
+  to_port = 22
+}
+
+resource "aws_eip" "nat-gateway" {
+  depends_on = [aws_internet_gateway.nat-gateway]
+
+}
+
+resource "aws_nat_gateway" "nat-gateway" {
+  allocation_id = aws_eip.nat-gateway.id
+  subnet_id     = aws_subnet.nat-gateway-public.id
+  depends_on    = [aws_internet_gateway.nat-gateway]
+  private_ip    = "10.0.0.20"
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_route_table_association" "nat-gateway-local" {
+  subnet_id      = aws_subnet.nat-gateway-private.id
+  route_table_id = aws_route_table.nat-gateway.id
+}
+
+resource "aws_route_table" "nat-gateway" {
+  vpc_id = aws_vpc.nat-gateway.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gateway.id
+  }
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_route_table_association" "internet-gateway" {
+  subnet_id      = aws_subnet.nat-gateway-public.id
+  route_table_id = aws_route_table.internet-gateway.id
+}
+
+resource "aws_route_table" "internet-gateway" {
+  vpc_id = aws_vpc.nat-gateway.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nat-gateway.id
+  }
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_instance" "nat-gateway" {
+  ami                    = "ami-0bd50a18ee156cba0"
+  instance_type          = "t2.micro"
+  availability_zone      = "eu-central-1a"
+  subnet_id              = aws_subnet.nat-gateway-private.id
+  private_ip             = "10.0.1.10"
+  key_name               = "ssh_aws_ed25519"
+  vpc_security_group_ids = [aws_security_group.nat-gateway.id]
+
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+resource "aws_instance" "nat-gateway-public" {
+  ami                    = "ami-0bd50a18ee156cba0"
+  instance_type          = "t2.micro"
+  availability_zone      = "eu-central-1a"
+  subnet_id              = aws_subnet.nat-gateway-public.id
+  associate_public_ip_address = true
+  private_ip             = "10.0.0.10"
+  key_name               = "ssh_aws_ed25519"
+  vpc_security_group_ids = [aws_security_group.nat-gateway.id]
+}
