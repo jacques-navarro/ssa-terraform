cf7dc39d860064c47487f1d6aaaef2015c05c489
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-06-01 08:20:23 +0200

    Add local name-prefix to #53

--- 53-create-auto-scaling-group/main.tf
+++ 53-create-auto-scaling-group/main.tf
@@ -2,17 +2,24 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+locals {
+  name-prefix = "asg-53"
+}
+
 resource "aws_launch_template" "asg-53-lt" {
-  name                   = "asg-53-lt"
+  name                   = "${local.name-prefix}-lt"
   image_id               = var.ami-id
   instance_type          = "t2.micro"
   key_name               = var.ssh-key
   vpc_security_group_ids = [var.security-group-id]
 
+  tags = {
+    name= "53-asg-lt"
+  }
 }
 
 resource "aws_autoscaling_group" "asg-53-asg" {
-  name               = "asg-53-asg"
+  name               = "${local.name-prefix}-asg"
   desired_capacity   = 2
   min_size           = 2
   max_size           = 2
@@ -22,4 +29,5 @@ resource "aws_autoscaling_group" "asg-53-asg" {
   launch_template {
     id = aws_launch_template.asg-53-lt.id
   }
+
 }
