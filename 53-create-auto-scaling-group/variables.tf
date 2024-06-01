8c7ac7d4840dbbe11e652a7d4eca9026d14a2613
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-06-02 00:01:33 +0200

    Remove web-access security group, AZ variables
    The security group variable referenced the 'web-access' security group
    which is attached to the default VPC. This caused a conflict when
    the custom VPC was introduced on 9e90bbb. Therefore, a new security group
    was introduced into the template.
    
    The 'availability-zones' variable seemed reduntant since Terraform
    doesn't allow you to refer to an entire list using a variable name.
    You still have to use index numbers when referencing individual
    availability zones.

--- 53-create-auto-scaling-group/variables.tf
+++ 53-create-auto-scaling-group/variables.tf
@@ -5,12 +5,3 @@ variable "ami-id" {
 variable "ssh-key" {
   default = "ssh_aws_ed25519"
 }
-
-variable "security-group-id" {
-  default = "sg-082f2af5714b40905"
-}
-
-variable "availability-zones" {
-  # type = list
-  default = ["eu-central-1a", "eu-central-1b"]
-}
