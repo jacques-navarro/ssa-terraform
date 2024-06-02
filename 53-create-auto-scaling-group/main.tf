cf9d098d23bf775f800e8c9ec01194b59ab00f66
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-06-02 14:56:30 +0200

    Add security group ingress rule tcp port 80

--- 53-create-auto-scaling-group/main.tf
+++ 53-create-auto-scaling-group/main.tf
@@ -93,6 +93,18 @@ resource "aws_vpc_security_group_ingress_rule" "asg-53-igr-ssh" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "asg-53-igr-http" {
+  security_group_id = aws_security_group.asg-53-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+
+  tags = {
+    name = "${local.name}-igr-ssh"
+  }
+}
+
 resource "aws_vpc_security_group_egress_rule" "asg-53-egr-ssh" {
   security_group_id = aws_security_group.asg-53-sg.id
   cidr_ipv4         = "0.0.0.0/0"
