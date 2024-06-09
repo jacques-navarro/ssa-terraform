4e6f7bd55be6daec027b4d9421a370544eaf6369
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-09 10:14:27 +0200

    Make changes in preparation for adding eu-west-1 provider
    These changes will makeit possible to add another AWS provider
    for the eu-west-1 region.
    
    - Add 'c1' alias to existing provider and add provider to resources
    - Add 'c1' suffix to existing local values that will not be common
      to both providers.
    - The following local values will be used by both providers and don't
      require the 'c1' suffix: name, cidr-local, cidr-all.

--- 86-vpc-peering/main.tf
+++ 86-vpc-peering/main.tf
@@ -1,48 +1,54 @@
 provider "aws" {
-  region = local.region
+  alias = "c1"
+  region = local.region-c1
 }
 
 locals {
-  region            = "eu-central-1"
   name              = "86-vpcp-"
-  name-suffix       = "-c1"
-  vpc-id            = aws_vpc.vpcp-86-vpc-c1.id
-  sg-id             = aws_security_group.vpcp-86-sg-c1.id
   cidr-local        = "10.0.0.0/16"
-  cidr-subnet       = "10.0.0.0/24"
   cidr-all          = "0.0.0.0/0"
-  availability-zone = "eu-central-1a"
+
+  region-c1            = "eu-central-1"
+  name-suffix-c1       = "-c1"
+  vpc-id-c1            = aws_vpc.vpcp-86-vpc-c1.id
+  sg-id-c1             = aws_security_group.vpcp-86-sg-c1.id
+  cidr-subnet-c1       = "10.0.0.0/24"
+  availability-zone-c1 = "eu-central-1a"
 }
 
 resource "aws_vpc" "vpcp-86-vpc-c1" {
+  provider = aws.c1
   cidr_block           = local.cidr-local
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${local.name}vpc${local.name-suffix}"
+    Name = "${local.name}vpc${local.name-suffix-c1}"
   }
 }
 
 resource "aws_subnet" "vpcp-86-subnet-c1" {
-  vpc_id            = local.vpc-id
-  cidr_block        = local.cidr-subnet
-  availability_zone = local.availability-zone
+  provider = aws.c1
+  vpc_id            = local.vpc-id-c1
+  cidr_block        = local.cidr-subnet-c1
+  availability_zone = local.availability-zone-c1
 
   tags = {
-    Name = "${local.name}subnet${local.name-suffix}"
+    Name = "${local.name}subnet${local.name-suffix-c1}"
   }
 }
 
 resource "aws_internet_gateway" "vpcp-86-ig-c1" {
-  vpc_id = local.vpc-id
+  provider = aws.c1
+  vpc_id = local.vpc-id-c1
 
   tags = {
-    Name = "${local.name}ig${local.name-suffix}"
+    Name = "${local.name}ig${local.name-suffix-c1}"
   }
 }
 
 resource "aws_route_table" "vpcp-86-rt-c1" {
-  vpc_id = local.vpc-id
+  provider = aws.c1
+  vpc_id = local.vpc-id-c1
 
   route {
     cidr_block = local.cidr-local
@@ -55,25 +61,28 @@ resource "aws_route_table" "vpcp-86-rt-c1" {
   }
 
   tags = {
-    Name = "${local.name}rt${local.name-suffix}"
+    Name = "${local.name}rt${local.name-suffix-c1}"
   }
 }
 
 resource "aws_route_table_association" "vpcp-86-rta-c1" {
+  provider = aws.c1
   route_table_id = aws_route_table.vpcp-86-rt-c1.id
   subnet_id      = aws_subnet.vpcp-86-subnet-c1.id
 }
 
 resource "aws_security_group" "vpcp-86-sg-c1" {
-  vpc_id = local.vpc-id
+  provider = aws.c1
+  vpc_id = local.vpc-id-c1
 
   tags = {
-    Name = "${local.name}sg${local.name-suffix}"
+    Name = "${local.name}sg${local.name-suffix-c1}"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "vpcp-86-igr-ssh-c1" {
-  security_group_id = local.sg-id
+  provider = aws.c1
+  security_group_id = local.sg-id-c1
 
   cidr_ipv4   = local.cidr-all
   from_port   = 22
@@ -81,12 +90,13 @@ resource "aws_vpc_security_group_ingress_rule" "vpcp-86-igr-ssh-c1" {
   to_port     = 22
 
   tags = {
-    Name = "${local.name}igr-ssh${local.name-suffix}"
+    Name = "${local.name}igr-ssh${local.name-suffix-c1}"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "vpc-86-igr-icmp" {
-  security_group_id = local.sg-id
+  provider = aws.c1
+  security_group_id = local.sg-id-c1
   cidr_ipv4         = local.cidr-local
   from_port         = 8
   ip_protocol       = "icmp"
@@ -94,14 +104,15 @@ resource "aws_vpc_security_group_ingress_rule" "vpc-86-igr-icmp" {
 }
 
 resource "aws_instance" "vpcp-86-ec2-c1" {
+  provider = aws.c1
   ami                         = var.ami-id
   instance_type               = var.instance-type
   subnet_id                   = aws_subnet.vpcp-86-subnet-c1.id
   associate_public_ip_address = true
   key_name                    = var.ssh-key
-  vpc_security_group_ids      = ["${local.sg-id}"]
+  vpc_security_group_ids      = ["${local.sg-id-c1}"]
 
   tags = {
-    Name = "${local.name}ec2${local.name-suffix}"
+    Name = "${local.name}ec2${local.name-suffix-c1}"
   }
 }
