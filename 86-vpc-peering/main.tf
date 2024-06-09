02d16f60cc98d7ceffec8139fae5c6d1500a43a2
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-09 09:57:23 +0200

    Add local configuration values
    This addition makes it easier to scale this template.

--- 86-vpc-peering/main.tf
+++ 86-vpc-peering/main.tf
@@ -1,54 +1,61 @@
 provider "aws" {
-  region = "eu-central-1"
+  region = local.region
 }
 
 locals {
-  name = "86-vpcp-"
-  vpc-id = aws_vpc.vpcp-86-vpc-c1.id
+  region            = "eu-central-1"
+  name              = "86-vpcp-"
+  name-suffix       = "-c1"
+  vpc-id            = aws_vpc.vpcp-86-vpc-c1.id
+  sg-id             = aws_security_group.vpcp-86-sg-c1.id
+  cidr-local        = "10.0.0.0/16"
+  cidr-subnet       = "10.0.0.0/24"
+  cidr-all          = "0.0.0.0/0"
+  availability-zone = "eu-central-1a"
 }
 
 resource "aws_vpc" "vpcp-86-vpc-c1" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = local.cidr-local
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${local.name}vpc-c1"
+    Name = "${local.name}vpc${local.name-suffix}"
   }
 }
 
 resource "aws_subnet" "vpcp-86-subnet-c1" {
-  vpc_id            = "${local.vpc-id}"
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "eu-central-1a"
+  vpc_id            = local.vpc-id
+  cidr_block        = local.cidr-subnet
+  availability_zone = local.availability-zone
 
   tags = {
-    Name = "${local.name}subnet-c1"
+    Name = "${local.name}subnet${local.name-suffix}"
   }
 }
 
 resource "aws_internet_gateway" "vpcp-86-ig-c1" {
-  vpc_id = "${local.vpc-id}"
+  vpc_id = local.vpc-id
 
   tags = {
-    Name = "${local.name}ig-c1"
+    Name = "${local.name}ig${local.name-suffix}"
   }
 }
 
 resource "aws_route_table" "vpcp-86-rt-c1" {
-  vpc_id = "${local.vpc-id}"
+  vpc_id = local.vpc-id
 
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = local.cidr-local
     gateway_id = "local"
   }
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = local.cidr-all
     gateway_id = aws_internet_gateway.vpcp-86-ig-c1.id
   }
 
   tags = {
-    Name = "${local.name}rt-c1"
+    Name = "${local.name}rt${local.name-suffix}"
   }
 }
 
