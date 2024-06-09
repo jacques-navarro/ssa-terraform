3790e0cd204aeae5440f35e10bf3bcd440fc978b
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-10 00:42:55 +0200

    Add VPC components for public subnet

--- 88-vpc-endpoint/main.tf
+++ 88-vpc-endpoint/main.tf
@@ -3,5 +3,62 @@ provider "aws" {
 }
 
 locals {
-  name = "88-vpce-"
+  name              = "88-vpce-"
+  name-suffix-pub   = "-pub"
+  vpc-id            = aws_vpc.vpce-88-vpc.id
+  availability-zone = "eu-central-1a"
+  cidr-local        = "10.0.0.0/16"
+  cidr-all          = "0.0.0.0/0"
+}
+
+resource "aws_vpc" "vpce-88-vpc" {
+  cidr_block           = local.cidr-local
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${local.name}vpc"
+  }
+}
+
+resource "aws_subnet" "vpce-88-subnet-pub" {
+  vpc_id            = local.vpc-id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = local.availability-zone
+
+  tags = {
+    Name = "${local.name}subnet${local.name-suffix-pub}"
+  }
+}
+
+resource "aws_internet_gateway" "vpce-88-ig" {
+  vpc_id = local.vpc-id
+
+  tags = {
+    Name = "${local.name}ig"
+  }
+}
+
+resource "aws_route_table" "vpce-88-rt-pub" {
+  vpc_id = local.vpc-id
+
+  route {
+    cidr_block = local.cidr-local
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = local.cidr-all
+    gateway_id = aws_internet_gateway.vpce-88-ig.id
+  }
+
+  tags = {
+    Name = "${local.name}rt${local.name-suffix-pub}"
+  }
+
+}
+
+resource "aws_route_table_association" "vpce-88-rta-pub" {
+  route_table_id = aws_route_table.vpce-88-rt-pub.id
+  subnet_id      = aws_subnet.vpce-88-subnet-pub.id
+
 }
