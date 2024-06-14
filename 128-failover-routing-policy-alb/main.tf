191430ad94648e1b16e50dc8a27f085233980933
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-14 09:52:36 +0200

    Add security group, launch template and auto-scaling group

--- 128-failover-routing-policy-alb/main.tf
+++ 128-failover-routing-policy-alb/main.tf
@@ -10,7 +10,7 @@ locals {
 }
 
 resource "aws_vpc" "frp-128-vpc-c1" {
-  cidr_block = local.cidr-local
+  cidr_block           = local.cidr-local
   enable_dns_hostnames = true
 
   tags = {
@@ -67,10 +67,103 @@ resource "aws_route_table" "frp-128-rt" {
 
 resource "aws_route_table_association" "frp-128-rta-pub1" {
   route_table_id = aws_route_table.frp-128-rt.id
-  subnet_id = aws_subnet.frp-128-sub-pub1.id
+  subnet_id      = aws_subnet.frp-128-sub-pub1.id
 }
 
-resource "aws_route_table_association" "frp-128-rta-pub2" {
-  route_table_id = aws_route_table.frp-128-rt.id
-  subnet_id = aws_subnet.frp-128-sub-pub2.id
+resource "aws_security_group" "frp-128-sg" {
+  vpc_id = local.vpc-id
+
+  tags = {
+    Name = "${local.name}rt"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "frp-128-igr-ssh" {
+  security_group_id = aws_security_group.frp-128-sg.id
+  cidr_ipv4         = local.cidr-all
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+
+  tags = {
+    Name = "${local.name}igr-ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "frp-128-ir-http" {
+  security_group_id = aws_security_group.frp-128-sg.id
+  cidr_ipv4         = local.cidr-all
+  from_port         = 1025
+  ip_protocol       = "tcp"
+  to_port           = 65535
+
+  tags = {
+    Name = "${local.name}igr-http"
+  }
+}
+
+resource "aws_launch_template" "frp-128-lt" {
+  name          = "128-frp-lt"
+  image_id      = "ami-06912d73bfa9ce345"
+  instance_type = "t2.micro"
+
+  key_name = "ssh_aws_ed25519"
+
+  network_interfaces {
+    associate_public_ip_address = true
+
+    # security group must assigned in network interface
+    security_groups = [aws_security_group.frp-128-sg.id]
+  }
+
+  user_data = filebase64("user_data.sh")
+
+  tags = {
+    Name = "${local.name}lt"
+  }
+}
+
+resource "aws_autoscaling_group" "frp-128-asg" {
+  name                = "128-frp-asg"
+  desired_capacity    = 2
+  min_size            = 2
+  max_size            = 2
+  vpc_zone_identifier = [aws_subnet.frp-128-sub-pub1.id, aws_subnet.frp-128-sub-pub2.id]
+  default_cooldown    = 60
+
+  launch_template {
+    id = aws_launch_template.frp-128-lt.id
+  }
+}
+
+resource "aws_lb_target_group" "frp-128-tg" {
+  name     = "128-frp-tg"
+  vpc_id   = local.vpc-id
+  port     = 80
+  protocol = "tcp"
+
+  tags = {
+    Name = "${local.name}lt"
+  }
+}
+
+resource "aws_lb" "frp-128-lb" {
+  name            = "128-frp-lb"
+  subnets         = [aws_subnet.frp-128-sub-pub1, aws_subnet.frp-128-sub-pub2]
+  security_groups = [aws_security_group.frp-128-sg.id]
+
+  tags = {
+    Name = "${local.name}lb"
+  }
+}
+
+resource "aws_lb_listener" "frp-128-lst" {
+  load_balancer_arn = aws_lb.frp-128-lb.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.frp-128-tg.id
+    type             = "forward"
+  }
 }
